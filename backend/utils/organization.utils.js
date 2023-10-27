132befe668ef9076e85913a5d9415270a776adca
janghede <markusjanghede@live.se>
2023-10-27 11:28:27 +0200

    Initial commit

---
+++ backend/utils/organization.utils.js
@@ -0,0 +1,31 @@
+/**
+ * Utility functions to get organizational data.
+ * No need to modify this to solve the slow organizations task.
+ */
+
+import { organizationLocations, organizationManagers, organizationNames } from "../mockup/organizations.mockup.js";
+
+export async function getOrganizationNames() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return organizationNames;
+}
+
+export async function getOrganizationLocations() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return organizationLocations;
+}
+
+export async function getOrganizationManagers() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return organizationManagers;
+}
+
+export function concatenateOrganizationalData(organizationNames, organizationLocations, organizationManagers) {
+  let result = [];
+  for (let orgName of organizationNames) {
+    let orgLoc = organizationLocations.find((orgLoc) => orgLoc.id == orgName.id);
+    let orgMan = organizationManagers.find((orgMan) => orgMan.id == orgName.id);
+    result.push({ id: orgName.id, name: orgName.name, location: orgLoc.location, manager: orgMan.manager });
+  }
+  return result;
+}
---
+++ frontend/src/views/SlowOrganizationsView.vue
@@ -0,0 +1,39 @@
+<script setup>
+import axios from "axios";
+import { ref, onMounted } from "vue";
+
+const orgs = ref([]);
+
+onMounted(() => {
+  axios.get("http://localhost:3007/api/organization/list").then((res) => {
+    orgs.value = res.data;
+  });
+});
+</script>
+
+<template>
+  <h4>Users</h4>
+  <div class="alert alert-secondary mt-3" role="alert">
+    <b>Task</b><br />
+    The data in this table is a concatenation of results from a few different sources. But due to some unoptimized promise resolving in the
+    backend service, the application is unnecessarily kept waiting. Modify the backend service so we don't have to wait as long.
+  </div>
+  <table class="table mt-3">
+    <thead>
+      <tr>
+        <th scope="col">id</th>
+        <th scope="col">name</th>
+        <th scope="col">location</th>
+        <th scope="col">manager</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="org in orgs">
+        <td>{{ org.id }}</td>
+        <td>{{ org.name }}</td>
+        <td>{{ org.location }}</td>
+        <td>{{ org.manager }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
