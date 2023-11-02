8beabe997ad8c39eb5dbcbfb21becea7ff364dbc
Oskar <oskar.fredriksson.external@fujitsu.com>
2023-11-02 10:43:54 +0100

    3/5

--- backend/service/organization.service.js
+++ backend/service/organization.service.js
@@ -1,13 +1,27 @@
 import * as organizationUtils from "../utils/organization.utils.js";
 
 export async function getOrganizationData() {
-  // Fetch data from source
-  const organizationNames = await organizationUtils.getOrganizationNames();
+    // Fetch data from source
+    const organizationNames = organizationUtils.getOrganizationNames();
 
-  const organizationLocations = await organizationUtils.getOrganizationLocations();
+    const organizationLocations = organizationUtils.getOrganizationLocations();
 
-  const organizationManagers = await organizationUtils.getOrganizationManagers();
+    const organizationManagers = organizationUtils.getOrganizationManagers();
 
-  // Concatenate organizational data into a tidy collection
-  return organizationUtils.concatenateOrganizationalData(organizationNames, organizationLocations, organizationManagers);
+    const [
+        organizationNamesResolve,
+        organizationLocationsResovle,
+        organizationManagersResolve,
+    ] = await Promise.all([
+        organizationNames,
+        organizationLocations,
+        organizationManagers,
+    ]);
+
+    // Concatenate organizational data into a tidy collection
+    return organizationUtils.concatenateOrganizationalData(
+        organizationNamesResolve,
+        organizationLocationsResovle,
+        organizationManagersResolve
+    );
 }
--- backend/service/user.service.js
+++ backend/service/user.service.js
@@ -1,25 +1,32 @@
 import { users } from "../mockup/user.mockup.js";
 
 export async function getUsers() {
-  await new Promise((resolve) => setTimeout(resolve, 4000));
-  return users;
+    await new Promise((resolve) => setTimeout(resolve, 4000));
+    return users;
 }
 
 export function getSummedAge() {
-  const summedAge = users.reduce((accumulator, user) => {
-    //Age can be a number or "unknown"
-    if (Number.isInteger(user.age)) {
-      return accumulator + user.age;
-    }
-  }, 0);
+    const summedAge = users.reduce((accumulator, user) => {
+        //Age can be a number or "unknown"
+        if (Number.isInteger(user.age)) {
+            return accumulator + user.age;
+        }
+        return accumulator;
+    }, 0);
 
-  return { result: `Summed age is ${summedAge}` };
+    return { result: `Summed age is ${summedAge}` };
 }
 
 export function getUsersIncludingApplications() {
-  var result = [];
-  users.forEach((user) => {
-    result.push({ ...user, applications: ["Microsoft Word (Installed)", "Adobe Photoshop (Software center)"] });
-  });
-  return result;
+    var result = [];
+    users.forEach((user) => {
+        result.push({
+            ...user,
+            applications: [
+                "Microsoft Word (Installed)",
+                "Adobe Photoshop (Software center)",
+            ],
+        });
+    });
+    return result;
 }
