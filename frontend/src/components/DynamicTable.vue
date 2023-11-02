8621ef26eea19326a191178dda4ce61e48f77101
Joakim Persson <joakim.persson@fujitsu.com>
2023-11-02 13:57:54 +0100

    Task 5 done

--- frontend/src/components/DynamicTable.vue
+++ frontend/src/components/DynamicTable.vue
@@ -1,24 +1,44 @@
 <script setup>
-import { computed } from "vue";
+import { computed, ref } from "vue";
 
 const props = defineProps(["data"]);
+const filterInput = ref("");
 
 const computedDataKeys = computed(() => {
-  return Object.keys(props.data[0]);
+    return Object.keys(props.data[0]);
+});
+
+const filteredData = computed(() => {
+    return props.data.filter((row) => {
+        const filterInputLower = filterInput.value.toLowerCase();
+
+        let match = false;
+
+        computedDataKeys.value.forEach((key) => {
+            console.log(key);
+            const column = row[key].toString().toLowerCase();
+            if (column.includes(filterInputLower)) {
+                match = true;
+            }
+        });
+
+        return match;
+    });
 });
 </script>
 
 <template>
-  <table class="table">
-    <thead>
-      <tr>
-        <th v-for="key in computedDataKeys">{{ key }}</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="dataRow in data">
-        <td v-for="key in computedDataKeys">{{ dataRow[key] }}</td>
-      </tr>
-    </tbody>
-  </table>
+    <input v-model="filterInput" placeholder="Filter" />
+    <table class="table">
+        <thead>
+            <tr>
+                <th v-for="key in computedDataKeys">{{ key }}</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="dataRow in filteredData">
+                <td v-for="key in computedDataKeys">{{ dataRow[key] }}</td>
+            </tr>
+        </tbody>
+    </table>
 </template>
